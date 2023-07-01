01079234748731c01719bb5f43f4e0242d24ee6e
k-brn <108495284+k-brn@users.noreply.github.com>
2023-07-01 02:13:31 -0700

    Add VendorList data file with fake vendor information

---
+++ fakeData.js
@@ -0,0 +1,123 @@
+// Fake data
+export const VendorList = [
+  {
+    id: 1,
+    name: "ABC Supplies",
+    email: "abc@supplies.com",
+    phone: 1234567890,
+    address: {
+      street: "123 Main Street",
+      city: "Cityville",
+      state: "Stateville",
+      country: "Countryland",
+    },
+  },
+  {
+    id: 2,
+    name: "XYZ Corporation",
+    email: "xyz@corp.com",
+    phone: 9876543210,
+    address: {
+      street: "456 Elm Street",
+      city: "Townville",
+      state: "Provinceland",
+      country: "Countryland",
+    },
+  },
+  {
+    id: 3,
+    name: "GHI Enterprises",
+    email: "ghi@enterprises.com",
+    phone: 5555555555,
+    address: {
+      street: "789 Oak Street",
+      city: "Villageville",
+      state: "Territoryland",
+      country: "Countryland",
+    },
+  },
+  {
+    id: 4,
+    name: "LMN Solutions",
+    email: "lmn@solutions.com",
+    phone: 1112223333,
+    address: {
+      street: "987 Maple Avenue",
+      city: "Hamletville",
+      state: "Regionland",
+      country: "Countryland",
+    },
+  },
+  {
+    id: 5,
+    name: "PQR Industries",
+    email: "pqr@industries.com",
+    phone: 4445556666,
+    address: {
+      street: "654 Pine Road",
+      city: "Metropolis",
+      state: "Districtland",
+      country: "Countryland",
+    },
+  },
+  {
+    id: 6,
+    name: "DEF Manufacturing",
+    email: "def@mfg.com",
+    phone: 7778889999,
+    address: {
+      street: "321 Cedar Lane",
+      city: "Villageland",
+      state: "Territoryland",
+      country: "Countryland",
+    },
+  },
+  {
+    id: 7,
+    name: "UVW Enterprises",
+    email: "uvw@enterprises.com",
+    phone: 6667778888,
+    address: {
+      street: "543 Oak Avenue",
+      city: "Citytown",
+      state: "Regionland",
+      country: "Countryland",
+    },
+  },
+  {
+    id: 8,
+    name: "RST Solutions",
+    email: "rst@solutions.com",
+    phone: 2223334444,
+    address: {
+      street: "876 Elm Lane",
+      city: "Hamletville",
+      state: "Districtland",
+      country: "Countryland",
+    },
+  },
+  {
+    id: 9,
+    name: "JKL Corporation",
+    email: "jkl@corp.com",
+    phone: 8889990000,
+    address: {
+      street: "789 Maple Avenue",
+      city: "Citytown",
+      state: "Provinceland",
+      country: "Countryland",
+    },
+  },
+  {
+    id: 10,
+    name: "MNO Services",
+    email: "mno@services.com",
+    phone: 3334445555,
+    address: {
+      street: "234 Pine Road",
+      city: "Villageland",
+      state: "Districtland",
+      country: "Countryland",
+    },
+  },
+];
